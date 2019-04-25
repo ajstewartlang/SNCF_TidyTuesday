b38185eb13a0d124c1fcd2a96012f4b9bde72206
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-04-25 22:23:51 +0100

    Create animation_code.R

---
+++ animation_code.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(ggthemes)
+library(tools)
+library(gganimate)
+library(ggimage)
+
+full_trains <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
+
+p <- full_trains %>%
+  filter(service == "International" & year == 2017) %>%
+  unite(journey, departure_station, arrival_station, sep = " - ") %>% 
+  mutate(journey = toTitleCase(tolower(journey))) %>%
+  mutate(month = factor(month, levels = 1:12, labels = month.name) ) %>%
+  mutate(image = "train.png") %>%
+  group_by(journey) %>%
+  arrange(month) %>% 
+  mutate(cum_sum = cumsum((num_late_at_departure))) %>% 
+  ggplot(aes(x = journey, y = cum_sum)) +
+  geom_image(aes(image = image), size = .15)  + 
+  guides(colour = FALSE) +
+  coord_flip() +
+  labs(title = "Running Total of International SNCF Trains Departing Late on Journeys", 
+       subtitle = "{closest_state} 2017", 
+       x = NULL, y = "Running Total of Trains Late at Departure") +
+  theme_economist() +
+  theme(plot.subtitle = element_text(size = 15), axis.title = element_text(size = 13), axis.text = element_text(size = 13)) +
+  transition_states(states = month)  
+
+animate(p, height = 500, width = 1000)
