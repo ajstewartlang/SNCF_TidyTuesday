f1e2d0c684482bb14f76ba8648ea98e09f26cd3e
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-05-01 14:35:26 +0100

    Update install.R

--- install.R
+++ install.R
@@ -2,5 +2,12 @@ install.packages("tidyverse")
 install.packages("ggthemes")
 install.packages("tools")
 install.packages("gganimate")
-install.packages("ggimage")
-install.packages("gifski")
+
+# Install packages from GitHub repos
+github_packages <- c("ImageOptim/gifski", “GuangchuangYu/ggimage”)
+ 
+for (g in github_packages) {
+if (! g %in% installed.packages()) {
+   devtools::install_github(g)
+}
+}
