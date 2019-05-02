cf55a9e65867899fe9669a193dfc8a53a8e9fd0a
Andrew Stewart <andrew.stewart@manchester.ac.uk>
2019-05-02 20:16:36 +0100

    Update animation_code.R

--- animation_code.R
+++ animation_code.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(ggthemes)
-library(tools)
 library(gganimate)
 library(ggimage)
+library(gifski)
 
 full_trains <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
 
